a6bf5c12c05c64283fd5cf4de1cfec855fb85c50
Fellipe Lyra <84457207+FellipeLyra@users.noreply.github.com>
2023-10-22 13:12:11 -0300

    Create Ex3.java

---
+++ Lista 1/Ex3.java
@@ -0,0 +1,19 @@
+package felipe;
+
+import java.util.Scanner;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.print("Digite o raio da esfera: ");
+        double raio = ler.nextDouble();
+
+        double pi = 3.14;
+        double volume = (4.0 / 3.0) * pi * Math.pow(raio, 3);
+
+        System.out.println(volume);
+
+        ler.close(); 
+    }
+}
